4b052a66be8f598ea8194fe72c10101ffd381c68
SengokuNadeko <hsun25874@gmail.com>
2026-04-07 17:14:39 -0400

    Fixed collisions, added combat_constants script to layout the collision layers

---
+++ scripts/combat_constants.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@export var layer_one = "world/static"
+@export var layer_two = "player hurtbox/body"
+@export var layer_three = "enemy hurtbox/body"
+@export var layer_four = "player attack"
+@export var layer_five = "enemy attack"
+@export var layer_six = "pickups"
